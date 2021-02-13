ea364e5f8958d9a9ad39753ca1bb2cb004d6b4ba
mahmoudsalah5294 <mahmoudsalah5294@gmail.com>
2021-02-13 22:56:13 +0200

    login layout

---
+++ app/src/main/java/com/mnm/mashawery/registration/Login.java
@@ -0,0 +1,16 @@
+package com.mnm.mashawery.registration;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.mnm.mashawery.R;
+
+public class Login extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login);
+    }
+}
